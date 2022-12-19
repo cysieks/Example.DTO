78963ec7751dbcd0112c834088f31d3ef7b23f24
Krzysztof Skiba <cysiek@gmail.com>
2022-12-19 14:57:43 +0100

    Initial commit

---
+++ User.vb
@@ -0,0 +1,11 @@
+Namespace Example.DTO
+    Public Class User
+        Public Property Id As Long
+
+        Public Property FirstName As String
+
+        Public Property LastName As String
+
+        Public Property Title As String
+    End Class
+End Namespace
